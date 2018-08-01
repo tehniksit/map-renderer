cbe0b39f90a5f22ec3046bda7e475c35b5226ac6
tehniksit <tehniksit@gmail.com>
2018-08-01 20:55:08 +0300

    import to db function added

--- app/app/services.py
+++ app/app/services.py
@@ -1,27 +1,65 @@
 from zipfile import ZipFile
- 
- 
+import os
+import ogr
+import gdal
+from .config import *
+
+gdal.SetConfigOption('CPL_DEBUG','ON')
+
+def testLoad(serverDS, table, sourceFile):
+	ogr.RegisterAll()
+	shapeDS = ogr.Open(sourceFile)
+	sourceLayer = shapeDS.GetLayerByIndex(0)
+	options = []
+	name = serverDS.CopyLayer(sourceLayer,table,options).GetName()
+
+
+def import_to_db(shapefile, tableName):
+	serverName = SERVERNAME
+	database = DB
+	port = PORT
+	usr = USER
+	pw = PASSWORD
+	table = tableName
+	connectionString = "PG:dbname='%s' host='%s' port='%s' user='%s' password='%s'" % (database,serverName,port,usr,pw)
+	ogrds = ogr.Open(connectionString)
+	name = testLoad(ogrds,table, shapefile)
+	
+
+def getShapefilename(path):
+
+	dirs = os.listdir(path)
+
+	for file in dirs:
+		if file.endswith('.shp'):
+			shapefile = str(file)
+
+	
+	return shapefile
+
+
 def upzip_file(file_name, path):
 
 	with ZipFile(file_name, 'r') as zip:
-		# printing all the contents of the zip file
-		#zip.printdir()
-	 
-		#print('Extracting all the files now...')
 		zip.extractall(path)
-		#print('Done!')
+
+	shapefileName = getShapefilename(path)	
+
+	shapefile = path + shapefileName
+
+	return shapefile
 
 
-
-def handle_uploaded_file(f, full_path, path):
+def handle_uploaded_file(f, full_path, folder):
 
 	with open(full_path, 'wb+') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
 
 	
-	upzip_file(full_path, path)
-	
+	shapefile = upzip_file(full_path, folder)
+	tableName = 'shape-table1'
+	import_to_db(shapefile, tableName)
 
-	return f.name
+	
 		
